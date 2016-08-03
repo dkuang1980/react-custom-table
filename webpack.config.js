f0f1f48af99f9a214ca4545b86d4aa31b6f42efe
Da Kuang <dak@surveymonkey.com>
2016-08-02 22:40:12 -0400

    configuration files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: [
+    './index.js'
+  ],
+   output: {
+      path: path.join(__dirname, 'build'),
+      publicPath: '/',
+      filename: '[name].js',
+      library: 'ReactSmartTable',
+      libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
